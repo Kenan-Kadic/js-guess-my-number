171aba62624bc3bb3f9df8f0cdec6751666c384e
Kenan Kadic <ken.kadic@outlook.com>
2022-06-15 10:17:39 -0500

    Dom Manipulation started

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// how to access a piece of information in HTML using querySelector
+// and to get the text
+
+// DOM allows you to access HTML ELEMENTS and styles to manipulate them.
+// It is a structured representation of HTML documents
+// DOM converts your HTML into a TREE STRUCTURE
+// You can easily read, access, and update the contents of the document
+// IT IS AN API and does this in memory
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent= 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
